a59b7a173a9e7de20ecc566c4ce74e7e9008cd46
Aadarsh Raj <68873194+aadarshraj4321@users.noreply.github.com>
2023-03-08 10:51:18 +0530

    Add files via upload

---
+++ basic_operations/mouse_clicks_and_events_in_OpenCV.py
@@ -0,0 +1,53 @@
+
+import cv2
+import  numpy as np
+
+
+
+##img = cv2.imread("image/1.jpg")
+##img = cv2.resize(img, (640, 400))
+##cv2.imshow("img", img)
+##cv2.waitKey(0)
+##cv2.destroyAllWindows()
+
+evt = 0
+def mouseClick(event, xPos, yPos, flags, params):
+    global evt
+    global pnt
+    if(event == cv2.EVENT_LBUTTONDOWN):
+        print("Mouse Event Was : ", event)
+        print("At Pos ", xPos, " ", yPos)
+        evt = event
+        pnt = (xPos, yPos)
+
+    if(event == cv2.EVENT_LBUTTONUP):
+        print("Mouse Event Was : ", event)
+        print("At Position ", xPos, " ", yPos)
+        evt = event
+
+    if(event == cv2.EVENT_RBUTTONUP):
+        print("RightButtonUP : ", event)
+        pnt = (xPos, yPos)
+        evt = event
+
+
+cap = cv2.VideoCapture("video/4.mp4")
+
+
+## listening mouse click
+cv2.namedWindow("frame")
+cv2.setMouseCallback("frame", mouseClick)
+while True:
+    ret, frame = cap.read()
+
+    if(evt == 1 or evt == 4):
+        cv2.circle(frame, pnt, 25, (0, 255, 255), 2)
+    cv2.imshow("frame", frame)
+    if(cv2.waitKey(1) & 0xff == ord('f')):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
